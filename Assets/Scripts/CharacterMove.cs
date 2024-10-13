a55833adbb13e1ecef0a47e7601d2a5b97808592
DevPatata <anavaro.sala@gmail.com>
2024-10-13 21:43:27 +0200

    input & movement [WIP]

--- Assets/Scripts/CharacterMove.cs
+++ Assets/Scripts/CharacterMove.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class CharacterMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public string horizontalInput = "Horizontal";
+public string verticalInput = "Vertical";
+public float speed = 6;
+public float rotSpeed = 20;
+private Rigidbody rig;
+private Vector2 input;
+private Vector3 movementVector;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+private void Start() {
+    rig = GetComponent<Rigidbody>();
+    rig.freezeRotation = true;
+}
+
+private void Update() {
+    input = new Vector2(Input.GetAxis(horizontalInput), Input.GetAxis(verticalInput));
+}
+
+private void FixedUpdate() {
+    movementVector = new Vector3(input.x, 0, input.y).normalized * speed;
+    rig.velocity = new Vector3 (movementVector.x, rig.velocity.y,  movementVector.z);
+
+    if (input != Vector2.zero) {
+        Vector3 targetDirection = new Vector3(input.x, 0, input.y);
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+        rig.rotation = Quaternion.Slerp(rig.rotation, targetRotation, rotSpeed * Time.fixedDeltaTime);
     }
+}
+
 }
