20e311d7c60668f2c424133f8c6420cf0e90ce88
DevPatata <anavaro.sala@gmail.com>
2024-10-13 22:15:36 +0200

    input & movement [WIP].2

--- Assets/Scripts/CharacterMove.cs
+++ Assets/Scripts/CharacterMove.cs
@@ -5,32 +5,32 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class CharacterMove : MonoBehaviour
 {
-public string horizontalInput = "Horizontal";
-public string verticalInput = "Vertical";
-public float speed = 6;
-public float rotSpeed = 20;
-private Rigidbody rig;
-private Vector2 input;
-private Vector3 movementVector;
+    public string horizontalInput = "Horizontal";
+    public string verticalInput = "Vertical";
+    public float speed = 6;
+    public float rotSpeed = 20;
+    private Rigidbody rig;
+    private Vector2 input;
+    private Vector3 movementVector;
 
-private void Start() {
-    rig = GetComponent<Rigidbody>();
-    rig.freezeRotation = true;
-}
-
-private void Update() {
-    input = new Vector2(Input.GetAxis(horizontalInput), Input.GetAxis(verticalInput));
-}
-
-private void FixedUpdate() {
-    movementVector = new Vector3(input.x, 0, input.y).normalized * speed;
-    rig.velocity = new Vector3 (movementVector.x, rig.velocity.y,  movementVector.z);
-
-    if (input != Vector2.zero) {
-        Vector3 targetDirection = new Vector3(input.x, 0, input.y);
-        Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
-        rig.rotation = Quaternion.Slerp(rig.rotation, targetRotation, rotSpeed * Time.fixedDeltaTime);
+    private void Start() {
+        rig = GetComponent<Rigidbody>();
+        rig.freezeRotation = true;
     }
-}
+
+    private void Update() {
+        input = new Vector2(Input.GetAxis(horizontalInput), Input.GetAxis(verticalInput));
+    }
+
+    private void FixedUpdate() {
+        movementVector = new Vector3(input.x, 0, input.y) * speed;
+        rig.velocity = new Vector3 (movementVector.x, rig.velocity.y,  movementVector.z);
+
+        if (input != Vector2.zero) {
+            Vector3 targetDirection = new Vector3(input.x, 0, input.y);
+            Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+            rig.rotation = Quaternion.Slerp(rig.rotation, targetRotation, rotSpeed * Time.fixedDeltaTime);
+        }
+    }
 
 }
