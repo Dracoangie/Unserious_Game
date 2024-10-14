2da88a1742d71b5b3d18028b6ac4e6529be42d4a
David <daviddelpinomorales33@gmail.com>
2024-10-14 11:27:45 +0200

    David_MovAng
    Implementacion de que se doblen.

--- Assets/Scripts/CharacterMove.cs
+++ Assets/Scripts/CharacterMove.cs
@@ -13,6 +13,7 @@ public class CharacterMove : MonoBehaviour
     private Vector2 input;
     private Vector3 movementVector;
     private bool controlActivado = true;
+    public float angRotSpeed = 10f;
 
 
     private void Start() {
@@ -21,19 +22,19 @@ public class CharacterMove : MonoBehaviour
     }
 
     private void Update() {
-        if (controlActivado)  // Solo aceptar input si control está activado
+        if (controlActivado)
         {
             input = new Vector2(Input.GetAxis(horizontalInput), Input.GetAxis(verticalInput));
         }
         else
         {
-            input = Vector2.zero; // No moverse si el control está desactivado
+            input = Vector2.zero;
         }
 
     }
 
     private void FixedUpdate() {
-        if (controlActivado)  // Solo mover si control está activado
+        if (controlActivado)
         {
             movementVector = new Vector3(input.x, 0, input.y) * speed;
             rig.velocity = new Vector3(movementVector.x, rig.velocity.y, movementVector.z);
@@ -42,12 +43,18 @@ public class CharacterMove : MonoBehaviour
             {
                 Vector3 targetDirection = new Vector3(input.x, 0, input.y);
                 Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+                float tiltX = Mathf.Abs(input.y) * angRotSpeed;
+                float tiltZ = Mathf.Abs(input.x) * angRotSpeed;
+
+                targetRotation *= Quaternion.Euler(tiltX + tiltZ, 0, 0);
+                Debug.Log(targetRotation);
+
+
                 rig.rotation = Quaternion.Slerp(rig.rotation, targetRotation, rotSpeed * Time.fixedDeltaTime);
             }
         }
         else
         {
-            // Asegurarse de que no hay movimiento
             rig.velocity = new Vector3(0, rig.velocity.y, 0);
         }
     }
