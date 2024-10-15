1434465bd8dfeef9c9d59f4399cee76f084a4383
David <daviddelpinomorales33@gmail.com>
2024-10-15 20:27:45 +0200

    David_MegaDeFixEscenaMaria
    Implementacion de la escena de Maria

---
+++ Assets/Scripts/ButtonInteraction.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Button3D : MonoBehaviour
+{
+    public Material normalMaterial;
+    public Material highlightedMaterial;
+    private Renderer objectRenderer;
+    public SceneTransition sceneTransition;
+
+    void Start()
+    {
+        objectRenderer = GetComponent<Renderer>();
+        objectRenderer.material = normalMaterial;
+    }
+
+    void OnMouseEnter()
+    {
+        objectRenderer.material = highlightedMaterial;
+    }
+
+    void OnMouseExit()
+    {
+        objectRenderer.material = normalMaterial;
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Boton presionado");
+        sceneTransition.FadeToScene("PlayScene");
+       // SceneManager.LoadScene("SampleScene");
+    }
+}
